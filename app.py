01a27421a623c169c4320d7abfca60891826a4f3
Kunal Sharda <kunalsharda@kunals-mbp-4.mynetworksettings.com>
2023-05-22 23:51:54 -0400

    Added types to some missing variables.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, typing
 from flask_wtf import CSRFProtect
+from typing import Any
 import os
 import tt_data_collect
 from tt_parse_data import parse_all_tts
@@ -7,25 +8,25 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-app = Flask(__name__)
-csrf_secret_key = os.urandom(32)
+app: Any = Flask(__name__)
+csrf_secret_key: bytes = os.urandom(32)
 app.config.update(dict(SECRET_KEY=csrf_secret_key))
-csrf = CSRFProtect(app)
+CSRFProtect(app)
 
-tiktok_data = {}
+tiktok_data: dict = {}
 
 
 @app.route('/', methods=('GET', 'POST'))
 def home() -> typing.ResponseReturnValue:  # put application's code here
-    form = tt_data_collect.TiktokForm()
-    validated = form.validate_on_submit()
+    form: tt_data_collect.TiktokForm = tt_data_collect.TiktokForm()
+    validated: bool = form.validate_on_submit()
     if validated:
         tiktok_data.update({"brows_hist": form.browsing.data,
                             "liked": form.liked.data,
                             "searched": form.searches.data,
                             "shared": form.shared.data,
                             "favorites": form.favorites.data})
-        print(parse_all_tts(tiktok_data, 3))
+        print(parse_all_tts(tiktok_data, 4))
     else:
         for field, errors in form.errors.items():
             for error in errors:
--- tt_parse_data.py
+++ tt_parse_data.py
@@ -4,16 +4,15 @@ import supabase as sb
 import os
 from datetime import datetime
 
-sb_url = os.environ.get("SUPABASE_URL")
-sb_key = os.environ.get("SUPABASE_KEY")
-sb_cli = sb.create_client(sb_url, sb_key)
+sb_url: str = os.environ.get("SUPABASE_URL")
+sb_key: str = os.environ.get("SUPABASE_KEY")
+sb_cli: sb.Client = sb.create_client(sb_url, sb_key)
 
 
 # Should send all the items to the server... pretty poor workflow however
 def parse_all_tts(entries: dict, limit: int) -> str:
     # Only need browsing history
     if entries["brows_hist"] is not None:
-
         # Find reference month, only using limited months of data
         first_line: str = entries["brows_hist"].readline().strip().decode("UTF-8")
         entries["brows_hist"].seek(0)
@@ -21,50 +20,52 @@ def parse_all_tts(entries: dict, limit: int) -> str:
 
         # Basic parsing of Date and ID + supabase and API calls
         current_video: dict = {}
-        final_list = []
+        final_list: list = []
+        api_response: str = ""
         for line in entries["brows_hist"]:
-            line = line.strip().decode("UTF-8")
+            line: str = line.strip().decode("UTF-8")
             # Can check lines here using print:
             if line.startswith("Date:"):
-                datetime_obj = datetime.strptime(line, "Date: %Y-%m-%d %H:%M:%S")
-                current_video["date"] = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+                datetime_obj: datetime = datetime.strptime(line, "Date: %Y-%m-%d %H:%M:%S")
+                current_video["date"]: str = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
             elif line.startswith("Link:"):
-                current_video["id"] = line.split("/")[-2].strip()
+                current_video["id"]: str = line.split("/")[-2].strip()
             elif line == "":
                 if "id" in current_video and "date" in current_video:
                     final_list.append(current_video.copy())
 
-                    check_limit = datetime.strptime(current_video["date"],
+                    check_limit: str = datetime.strptime(current_video["date"],
                                                     "%Y-%m-%d %H:%M:%S").strftime("%m")
                     if int(check_limit) + limit < ref_month:
-                        sb_cli.table("tiktoks").upsert({"id": current_video["id"],
-                                                        "viewed_at": current_video["date"]},
-                                                       on_conflict="id").execute()
+                        api_response = str(sb_cli.table("tiktoks").upsert({"id": current_video["id"],
+                                                                           "viewed_at": current_video["date"]},
+                                                                          on_conflict="id").execute())
+
                         break
                     current_video.clear()
     else:
         raise FileNotFoundError("This should not happen.")
 
-    return "Finished sending all Tik Toks!"
+    return "Finished sending all Tik Toks! " + api_response
 
 
-def parse_personal_tts(entries: dict, limit: int) -> str:
+def parse_personal_tts(entries: dict, limit: int, acc_id: int) -> str:
     # Liked Videos
     if entries["liked"] is not None:
-        data = entries["liked"]
+        print("Liked!")
     else:
         raise FileNotFoundError("This should not happen.")
 
     # Searched Videos (not required)
     if entries["searched"] is not None:
-        data = entries["searched"]
+        print("Searched!")
 
     # Shared Videos (not required)
     if entries["shared"] is not None:
-        data = entries["shared"]
+        print("Shared!")
 
     # Favorite Videos (not required)
     if entries["favorites"] is not None:
-        data = entries["favorites"]
+        print("Favorited")
 
     return "done parsing!"
